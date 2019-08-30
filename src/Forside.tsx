f19c8b0d265f8b1b1330b2881d3297d89dab6275
André Wolden <wolden.andre@gmail.com>
2019-08-30 10:03:31 +0200

    asdf

--- src/Forside.tsx
+++ src/Forside.tsx
@@ -12,6 +12,7 @@ import ReactJson from "react-json-view";
 import {Hendelse} from "./types/foo";
 import {mergeListsToLengthN} from "./utils/utilityFunctions";
 import Ekspanderbartpanel from "nav-frontend-ekspanderbartpanel";
+import Lesmerpanel from "nav-frontend-lesmerpanel";
 
 // const additionalMetaSchemas = require("ajv/lib/refs/json-schema-draft-06.json");
 const initialHendelseTest = require('./digisos/initial-hendelse-test');
@@ -109,56 +110,64 @@ class Forside extends React.Component<Props, ForsideState> {
         const {appname} = this.props.example;
 
         const listOfJsxHendelser = hendelserKomplett.map((hendelse: Hendelse, idx: number) => {
-
             const {historyPoint} = this.state;
-            const buttonText = this.state.hendelserPrepared.length > idx
-                ? "Hit"
-                : "Hit";
-
-            let buttonBackgroundColor: string = "grey";
+            let buttonBackgroundColor: string;
+            let buttonIcon: JSX.Element;
 
             if ( idx > historyPoint){
                 buttonBackgroundColor = "white";
+                buttonIcon = (<span className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>)
             } else if (idx === historyPoint) {
                 buttonBackgroundColor = "red";
+                buttonIcon = (<span className="glyphicon glyphicon-ok" aria-hidden="true"></span>)
+            } else {
+                buttonBackgroundColor = "grey";
+                buttonIcon = (<span className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>)
             }
 
+            const intro: JSX.Element = (
+                <>
+                    <p>{hendelse.type}</p>
+                    <Knapp
+                        style={{backgroundColor: buttonBackgroundColor}}
+                        onClick={() => this.handleChooseHistoryPoint(idx)}
+                    >
+                        { buttonIcon }
+                    </Knapp>
+                </>
+            );
+
             return (
-                <li key={idx}>
-                    <Panel>
-                        <p>{hendelse.type}</p>
-                        <Knapp
-                            disabled={idx === 0}
-                            style={{backgroundColor: buttonBackgroundColor}}
-                            onClick={() => this.handleChooseHistoryPoint(idx)}
-                        >
-                            {buttonText}
-                        </Knapp>
-                        <div style={{display: "inline-flex"}}>
-                            <Ekspanderbartpanel tittel={"Rediger element"} border>
-                                <Form schema={hendelseSchema}
-                                      formData={hendelse}
-                                      // onChange={(json) => this.handleChange(json)}
-                                      // onSubmit={(json) => this.handleSubmit(json)}
-                                      // onError={log("errors")}
-                                />
-                            </Ekspanderbartpanel>
-                        </div>
-                    </Panel>
+                <li key={idx} className={"margintop"}>
+                    <Lesmerpanel intro={intro} border>
+                        <Form schema={hendelseSchema}
+                              formData={hendelse}
+                              // onChange={(json) => this.handleChange(json)}
+                              // onSubmit={(json) => this.handleSubmit(json)}
+                              // onError={log("errors")}
+                        />
+                    </Lesmerpanel>
                 </li>
             );
         });
 
+
         return (
             <div className={"margintop"}>
                 <Panel>
-                    <h3>
-                        Bruker- og søknadsdata
-                    </h3>
-                    <SkjemaGruppe>
-                        <Input label={'Bruker identifikator'}/>
-                        <Input label={'Søknadsreferanse'}/>
-                    </SkjemaGruppe>
+                    <div className={"columns"}>
+                        <div style={{width: "30%"}} className={"columns__column"}>
+                            <h3>
+                                Bruker- og søknadsdata
+                            </h3>
+                        </div>
+                        <div style={{width: "30%"}} className={"columns__column"}>
+                            <Input label={'Bruker identifikator'}/>
+                        </div>
+                        <div style={{width: "30%"}} className={"columns__column"}>
+                            <Input label={'Søknadsreferanse'}/>
+                        </div>
+                    </div>
                 </Panel>
 
                 <Panel>
