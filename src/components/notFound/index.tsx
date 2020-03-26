1cb3d813196315faa73812d54ea7546a1d4479cb
Halvor Bjørn <halvor.bjorn@nav.no>
2020-03-26 14:56:17 +0100

    Fjerner nav-frontend-moduler

--- src/components/notFound/index.tsx
+++ src/components/notFound/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import {Panel} from "nav-frontend-paneler";
 
 const NotFound = () => {
     return(
-        <Panel>
-            <div style={{textAlign: "center"}}>
-                <p>This is not the web page you are looking for....</p>
-                <img src={"img/404.png"} alt={""} width={"600px"}/>
-            </div>
-        </Panel>
+        <div style={{textAlign: "center"}}>
+            <p>This is not the web page you are looking for...</p>
+            <img src={"img/404.png"} alt={""} width={"600px"}/>
+        </div>
     )
 };
 
