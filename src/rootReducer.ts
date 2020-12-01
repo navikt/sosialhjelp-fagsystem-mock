59cc5bc50950eba59e56ca2d551df93bbc11522e
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-12-01 09:48:01 +0100

    Oppgraderer react-scripts til 4.0.1 (#34)

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from "redux";
-import { connectRouter } from 'connected-react-router'
+import { connectRouter } from "connected-react-router";
 import exampleReducer from "./redux/example/exampleReducer";
-import Reducer from "./redux/reducer";
+import modelReducer from "./redux/reducer";
 
-export default (history: any) => combineReducers({
-	router: connectRouter(history),
-	example: exampleReducer,
-	model: Reducer
-});
+const reducer = (history: any) =>
+  combineReducers({
+    router: connectRouter(history),
+    example: exampleReducer,
+    model: modelReducer,
+  });
+
+export default reducer;
