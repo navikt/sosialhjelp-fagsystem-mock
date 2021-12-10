df068cca59e240d7c800ef9567ad8f4da3d241b2
linemos <line.moseng@nav.no>
2021-12-10 13:07:28 +0100

    Fyll ut alle felter for dokumentasjon etterspurt (#62)
    * Autoutfyll dokumentasjon etterspurt
    
    * unik key på rader

--- src/pages/parts/dokumentasjonEtterspurt/NyDokumentasjonEtterspurtModal.tsx
+++ src/pages/parts/dokumentasjonEtterspurt/NyDokumentasjonEtterspurtModal.tsx
@@ -2,6 +2,7 @@ import React, {useRef, useState} from 'react';
 import {AppState, DispatchProps} from "../../../redux/reduxTypes";
 import {connect} from "react-redux";
 import {createStyles, Modal, Paper, Theme} from "@material-ui/core";
+import {v4 as uuidv4} from 'uuid';
 import {
     oppdaterDokumentasjonEtterspurt,
     sendNyHendelseOgOppdaterModel,
@@ -16,9 +17,18 @@ import 'date-fns';
 import Typography from "@material-ui/core/Typography";
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
-import {Dokument, DokumentasjonEtterspurt, FilreferanseType, HendelseType} from "../../../types/hendelseTypes";
+import {
+    Dokument,
+    DokumentasjonEtterspurt,
+    FilreferanseType,
+    HendelseType
+} from "../../../types/hendelseTypes";
 import Grid from "@material-ui/core/Grid";
-import {formatDateString, getDateOrNullFromDateString, getNow} from "../../../utils/utilityFunctions";
+import {
+    formatDateString,
+    getDateOrNullFromDateString,
+    getNow
+} from "../../../utils/utilityFunctions";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import Table from "@material-ui/core/Table";
@@ -233,8 +243,20 @@ const NyDokumentasjonEtterspurtModal: React.FC<Props> = (props: Props) => {
         }
     };
 
+    const setDefaultDokumentasjonEtterspurt = () => {
+        const nyttDokument: Dokument = {
+            dokumenttype: 'Tannlege',
+            tilleggsinformasjon: 'Kostnadsoverslag fra tannlegen din ',
+            innsendelsesfrist: getNow(),
+            dokumentreferanse: createDokumentreferanse(),
+        };
+        setModalDokumentasjonEtterspurt({...modalDokumentasjonEtterspurt, dokumenter: [...modalDokumentasjonEtterspurt.dokumenter, nyttDokument]});
+        setModalDokument({...initialDokument});
+    };
+
     const makeTableRow = (dokument: Dokument, idx:number) => {
-        return <TableRow key={dokument.dokumenttype + dokument.tilleggsinformasjon}>
+        const uuid = uuidv4();
+        return <TableRow key={dokument.dokumenttype + dokument.tilleggsinformasjon + uuid}>
             <TableCell component="th" scope="row">
                 {dokument.dokumenttype}
             </TableCell>
@@ -368,6 +390,13 @@ const NyDokumentasjonEtterspurtModal: React.FC<Props> = (props: Props) => {
                         </div>
                         <div className={classes.paperback2}>
                             <Box className={classes.addbox}>
+
+                                <Button variant="outlined" color={'default'} onClick={() => {
+                                    setDefaultDokumentasjonEtterspurt();
+                                }}>
+                                    Fyll ut alle felter
+                                </Button>
+
                                 <Button id={"etterspor_dokumentasjon_send"} variant={!dokumenterErAlleredeEtterspurt ? 'contained' : 'outlined'}
                                         color={!dokumenterErAlleredeEtterspurt ? 'primary' : 'secondary'} onClick={() => {
                                     sendDokumentasjonEtterspurt();
