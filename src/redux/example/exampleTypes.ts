8e4ee5c1906afb2f4b3b8a521387fdf73004cdab
Martin Almvik <martin.almvik@nav.no>
2022-12-08 14:10:40 +0100

    Kjørte prettier -w

--- src/redux/example/exampleTypes.ts
+++ src/redux/example/exampleTypes.ts
@@ -1,16 +1,14 @@
 export interface ExampleModel {
-    appname: string;
+  appname: string;
 }
 
-
-export type ExampleAction
-    = SetAppName
+export type ExampleAction = SetAppName;
 
 export enum ExampleActionTypeKeys {
-    SET_APP_NAME = "SET_APP_NAME"
+  SET_APP_NAME = "SET_APP_NAME",
 }
 
 export interface SetAppName {
-    type: ExampleActionTypeKeys.SET_APP_NAME,
-    appname: string
+  type: ExampleActionTypeKeys.SET_APP_NAME;
+  appname: string;
 }
--- src/types/additionalTypes.ts
+++ src/types/additionalTypes.ts
@@ -1,5 +1,4 @@
-
 export interface NavKontor {
-    id: string,
-    name: string
+  id: string;
+  name: string;
 }
