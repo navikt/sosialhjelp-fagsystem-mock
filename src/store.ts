474ea473ea5d411d4f7cabec70ae66807f4be225
joeldlin <jorgen.elden.lindgren@nav.no>
2025-12-03 11:35:16 +0100

    fikser dispatch error

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modelReducer from "./redux/reducer";
+import { useDispatch, useSelector } from "react-redux";
 
 const store = configureStore({ reducer: { model: modelReducer } });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
 
 export default store;
